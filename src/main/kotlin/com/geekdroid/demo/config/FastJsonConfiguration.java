7280dcd5a5556bb2d103d144477bd2af6c42d213
james.li <james.li@wesoft.com>
2018-09-04 16:34:55 +0800

    enable fastjson configuration.

--- src/main/kotlin/com/geekdroid/demo/config/FastJsonConfiguration.java
+++ src/main/kotlin/com/geekdroid/demo/config/FastJsonConfiguration.java
@@ -5,7 +5,9 @@ import com.alibaba.fastjson.serializer.ValueFilter;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter4;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class FastJsonConfiguration {
 
     @Bean
